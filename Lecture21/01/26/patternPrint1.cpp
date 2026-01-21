5be1fc775902c6cb3795513c93a483be0c89b1b5
Techwithprabha02 <yprabha0211yadav@gmail.com>
2026-01-21 22:34:57 +0530

    Initial commit

---
+++ Lecture21/01/26/patternPrint1.cpp
@@ -0,0 +1,18 @@
+// Ques 1:
+// n = 5
+// * * * * *
+// * * * * *
+// * * * * *
+// * * * * *
+// * * * * *
+#include<iostream>
+using namespace std;
+int main() {
+    int n;
+    for(int i=0;i<5;i++) {
+        for(int i=0;i<5;i++){
+            cout<<"*"<<" ";
+        }
+        cout<<endl;
+    }
+}
