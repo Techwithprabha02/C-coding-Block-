bb0508b1e2d8b0c7c36fe591348470a73c1750eb
Techwithprabha02 <yprabha0211yadav@gmail.com>
2026-02-03 11:30:21 +0530

    Added program

--- Lecture2/02/26/BinarytoDeci.cpp
+++ Lecture2/02/26/BinarytoDeci.cpp
@@ -1,34 +1,16 @@
-// #include<iostream>
-// using namespace std;
-
-// int main() {
-//     int binary,decimal=0,base=1;
-//     cin>>binary;
-
-//     while(binary>0) {
-//         int n=binary%10;
-//         decimal=decimal+n*base;
-//         base=base*2;
-//         binary=binary/10;
-//     }
-//     cout<<decimal;
-//     return 0;
-// }
 #include<iostream>
 using namespace std;
+
 int main() {
-	int N;
-	int rev=0;
+    int binary,decimal=0,base=1;
+    cin>>binary;
 
-	cin>>N;
-
-	while(N>0) {
-		int rem=N%10;
-        rev=rev*10+rem;
-		N=N/10;
-
-	}
-	cout<<rev;
-
-	return 0;
+    while(binary>0) {
+        int n=binary%10;
+        decimal=decimal+n*base;
+        base=base*2;
+        binary=binary/10;
+    }
+    cout<<decimal;
+    return 0;
 }
